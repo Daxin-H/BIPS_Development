dab7af448c32d8e939472ce6c66ede397f32b18d
Daxin-H <huangd5@rpi.edu>
2022-10-24 11:27:37 -0400

    testing

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+var current = new Date();
+app.get('/', (req, res) => {
+
+    res
+        .status(201) // 201 = Created
+        .render(current.now())
+        .set({
+            'X-Custom-Header': "I'm a header value!",
+            'Content-Type': 'text/html',
+        })
+        .send('<h1>Some HTML!</h1>');
+});
+
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
