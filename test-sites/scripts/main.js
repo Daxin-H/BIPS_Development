7b6f37aa8af141a0f805bd759c1feb39119dd50e
Daxin-H <huangd5@rpi.edu>
2022-10-25 01:24:54 -0400

    sds

--- test-sites/scripts/main.js
+++ test-sites/scripts/main.js
@@ -1,15 +1,9 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-// document.querySelector("html").addEventListener("click", function () {
-//     alert("Ouch! Stop poking me!");
-// });
+
 var d = new Date();
+
 var hour = d.getHours();
 var mint = d.getMinutes();
 var sec = d.getSeconds();
-
-// myContent.textContent = "Current tims is -> " + hour + ":" + mint + ":" + mili;
-// myContent.textContent = "tims is -> " + d.getMilliseconds();
 
 const myContent = document.querySelector("h2");
 myContent.textContent = "Current tims is -> " + hour + ":" + mint + ":" + sec;
