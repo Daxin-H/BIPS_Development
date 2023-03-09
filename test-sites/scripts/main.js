cc12c9ddb3bdf443eb8cfcecf9c2aad6f1d91c50
Daxin-H <huangd5@rpi.edu>
2023-03-09 03:17:11 -0500

    Revert "chart"
    This reverts commit 6acc172c073d8f976ece23888c4af60c325c4ea4.

--- test-sites/scripts/main.js
+++ test-sites/scripts/main.js
@@ -1,84 +1,60 @@
-const Fetch_Issue = async (out_num) => {
-  const data = await fetch(`https://api.github.com/repos/google/guava/issues?per_page=2000&page=1`)
-    .then((response) => response.json())
-    .then((data) => {
-      const NameContainer = document.querySelector('.username');
-      const countings = {
-        p3: 0,
-        type: 0,
-        package: 0,
-        java: 0,
-      };
+var d = new Date();
+var hour = d.getHours();
+var mint = d.getMinutes();
+var sec = d.getSeconds();
 
+const myHeader = document.querySelector("h1");
+// const myContent = document.querySelector("h2");
+// myContent.textContent = "Current tims is -> " + hour + ":" + mint + ":" + sec;
+
+
+const gitHubForm = document.getElementById('gitHubForm');
+const NameContainer = document.querySelector(".username");
+
+// const Fetch_Issue = async (issue) => {
+//   const Issue_api = await fetch(`https://api.github.com/repos/google/guava/issues`);
+//   const issue_data = await Issue_api.json();
+
+//   console.log(issue_data);
+//   return issue_data;
+// }
+
+
+const countings = {
+  'p3': 0,
+  'type': 0,
+  'package': 0,
+  'java': 0,
+
+}
+
+const Fetch_Issue = (out_num) => {
+  fetch(`https://api.github.com/repos/google/guava/issues`)
+    .then(response => response.json())
+    .then(data => {
       count_temp = formatResults(data, countings);
       console.log(countings);
-      NameContainer.innerHTML = `<p><strong> P3:${countings['p3']} </p>
-      <p><strong> type:${countings['type']}</p>
-      <p><strong> java:${countings['java']}</p>
-      <p><strong> package:${countings['package']}</p>
+      NameContainer.innerHTML = `<p><strong> P3:${countings['p3']} </p> 
+      <p><strong> type:${countings['type']}</p> 
+      <p><strong> java:${countings['java']}</p> 
+      <p><strong> package:${countings['package']}</p> 
       `;
       out_num = countings;
-
-      const ctx = document.getElementById('myChart').getContext('2d');
-      const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: [],
-          datasets: [
-            {
-              label: '# of Votes',
-              data: [],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-
-      console.log(data);
-      // process result
-
-      myChart.data.datasets[0].data = Object.keys(countings).map(
-        (key) => countings[key]
-      );
-      myChart.data.labels = Object.keys(countings);
-
-      myChart.update();
     })
-    .catch((error) => console.error(error));
+    .catch(error => console.error(error))
+
 };
 
 const formatResults = (data, countings) => {
   // Loop over each object in data array
   for (let i in data) {
     for (let j in data[i].labels) {
-      const issue = data[i].labels[j];
+      const issue = data[i].labels[j]
 
-      if (issue.name.includes('package')) {
+      if (issue.name.includes("package")) {
         countings['package'] += 1;
       }
-      if (issue.name.includes('type')) {
+      if (issue.name.includes("type")) {
         countings['type'] += 1;
       }
       if (issue.name === 'java') {
@@ -89,6 +65,7 @@ const formatResults = (data, countings) => {
       }
     }
   }
+
 };
 
 const out_num = {};
